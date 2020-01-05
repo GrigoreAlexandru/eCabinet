c595d9784a4d0f7dc69ee051a9fa0beffb34c14f
mudbloom <grigorealexandru.dev@gmail.com>
2020-01-05 19:26:10 +0200

    up

--- src/main/java/Grigilia/App.java
+++ src/main/java/Grigilia/App.java
@@ -1,13 +1,17 @@
 package Grigilia;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+            DB.init();
+            // Account acc = new Account("test", "psdd");
+            // Account acc2 = new Account("t2est", "psdd");
+            // DB.saveAccount(acc);
+            // DB.saveAccount(acc2);
+            System.out.println(login("test", "psdd"));
+            DB.saveDb();
+    }
+
+    private static boolean login(String usr, String pwd){
+        return DB.getAccounts().contains(new Account(usr,pwd));
     }
 }
--- src/main/java/Grigilia/DB.java
+++ src/main/java/Grigilia/DB.java
@@ -1,16 +1,63 @@
 package Grigilia;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.HashSet;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 class DB {
 
-    private static ArrayList<Account> accounts;
+    private static HashSet<Account> accounts;
+    private static Gson gson = new GsonBuilder().create();
+    private static File accountsFile = new File("accounts");
+         
 
-    public static ArrayList<Account> getAccounts() {
+    public static void init() {
+        if (accountsFile.exists()) {
+            Scanner sc;
+            try {
+                sc = new Scanner(accountsFile);
+                final String json = sc.nextLine();
+                java.lang.reflect.Type founderSetType = new TypeToken<HashSet<Account>>(){}.getType();
+
+                accounts = gson.fromJson(json, founderSetType);  
+                // accounts = gson.fromJson(json, HashSet.class);
+                if (accounts == null) {
+                    accounts = new HashSet<Account>();
+                }
+                System.out.println(accounts);
+                System.out.println(accounts.size());
+            } catch (final FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        } else {
+            saveDb();
+            init();
+        }
+    }
+
+    public static void saveAccount(final Account account) {
+        accounts.add(account);
+    }
+
+    public static HashSet<Account> getAccounts() {
         return accounts;
     }
 
-    public static void setAccounts(ArrayList<Account> accounts) {
-        DB.accounts = accounts;
+    public static void saveDb() {
+        try (Writer writer = new FileWriter(accountsFile)) {
+            gson.toJson(accounts, writer);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    
 }
